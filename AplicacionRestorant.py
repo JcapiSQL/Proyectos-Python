40cc41a5cd8faaef8918bc89528d50a566f52352
CapiDev <juannicolascapitan77@gmail.com>
2024-12-13 17:35:44 -0300

    Update AplicacionRestorant.py

--- AplicacionRestorant.py
+++ AplicacionRestorant.py
@@ -1,1 +1,553 @@
+from tkinter import *
+import random
+import datetime
+from tkinter import filedialog, messagebox
+
+operador = ''
+precios_comida = [1.66, 2.10, 1.80, 3.21, 1.94, 2.35, 1.74, 3.14]
+precios_bebidas = [1.44, 1.10, 1.20, 2.21, 1.84, 1.37, 2.44, 1.14]
+precios_postres = [1.12, 2.40, 2.26, 1.21, 2.74, 1.45, 2.64, 1.54]
+
+
+def click_boton(numero):
+    global operador
+    operador = operador + numero
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(END, operador)
+
+
+def borrar():
+    global operador
+    operador = ''
+    visor_calculadora.delete(0, END)
+
+
+def obtener_reultado():
+    global operador
+    resultado = str(eval(operador))
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(0, resultado)
+    operador = ''
+
+
+def revisar_check():
+    x = 0
+    for c in cuadros_comida:
+        if variables_comida[x].get() == 1:
+            cuadros_comida[x].config(state=NORMAL)
+            if cuadros_comida[x].get() == '0':
+                cuadros_comida[x].delete(0, END)
+                cuadros_comida[x].focus()
+        else:
+            cuadros_comida[x].config(state=DISABLED)
+            texto_comida[x].set('0')
+        x += 1
+
+    x = 0
+    for c in cuadros_bebida:
+        if variables_bebida[x].get() == 1:
+            cuadros_bebida[x].config(state=NORMAL)
+            if cuadros_bebida[x].get() == '0':
+                cuadros_bebida[x].delete(0, END)
+                cuadros_bebida[x].focus()
+        else:
+            cuadros_bebida[x].config(state=DISABLED)
+            texto_bebida[x].set('0')
+        x += 1
+
+    x = 0
+    for c in cuadros_postres:
+        if variables_postres[x].get() == 1:
+            cuadros_postres[x].config(state=NORMAL)
+            if cuadros_postres[x].get() == '0':
+                cuadros_postres[x].delete(0, END)
+                cuadros_postres[x].focus()
+        else:
+            cuadros_postres[x].config(state=DISABLED)
+            texto_postres[x].set('0')
+        x += 1
+
+def total():
+    sub_total_comida = 0
+    p = 0
+    for cantidad in texto_comida:
+        sub_total_comida = sub_total_comida + (float(cantidad.get()) * precios_comida[p])
+        p += 1
+
+    sub_total_bebidas = 0
+    p = 0
+    for cantidad in texto_bebida:
+        sub_total_bebidas = sub_total_bebidas + (float(cantidad.get()) * precios_bebidas[p])
+        p += 1
+
+    sub_total_postres = 0
+    p = 0
+    for cantidad in texto_postres:
+        sub_total_postres = sub_total_postres + (float(cantidad.get()) * precios_postres[p])
+        p += 1
+
+    sub_total = sub_total_comida + sub_total_bebidas + sub_total_postres
+    impuestos = sub_total * 0.07
+    total = sub_total + impuestos
+
+    var_costo_comida.set(f"$ {round(sub_total_comida, 2)}")
+    var_costo_bebida.set(f"$ {round(sub_total_bebidas, 2)}")
+    var_costo_postre.set(f"$ {round(sub_total_postres, 2)}")
+    var_subtotal.set(f"$ {round(sub_total, 2)}")
+    var_impuesto.set(f"$ {round(impuestos, 2)}")
+    var_total.set(f"$ {round(total, 2)}")
+
+
+def recibo():
+    texto_recibo.delete(1.0, END)
+    num_recibo = f'N# - {random.randint(1000, 9999)}'
+    fecha = datetime.datetime.now()
+    fecha_recibo = f'{fecha.day}/{fecha.month}/{fecha.year} - {fecha.hour}:{fecha.minute}'
+    texto_recibo.insert(END, f'Datos:\t{num_recibo}\t\t{fecha_recibo}\n')
+    texto_recibo.insert(END, f'*' * 63 + '\n')
+    texto_recibo.insert(END, f'Items\t\tCant.\tCosto Items\n')
+    texto_recibo.insert(END, f'-' * 70 + '\n')
+
+    x = 0
+    for comida in texto_comida:
+        if comida.get() != '0':
+            texto_recibo.insert(END, f'{lista_comidas[x]}\t\t{comida.get()}\t'
+                                     f'${round(float(int(comida.get()) * precios_comida[x]),2)}\n')
+        x += 1
+
+    x = 0
+    for bebida in texto_bebida:
+        if bebida.get() != '0':
+            texto_recibo.insert(END, f'{lista_bebidas[x]}\t\t{bebida.get()}\t'
+                                     f'${round(float(int(bebida.get()) * precios_bebidas[x]),2)}\n')
+        x += 1
+
+    x = 0
+    for postres in texto_postres:
+        if postres.get() != '0':
+            texto_recibo.insert(END, f'{lista_postres[x]}\t\t{postres.get()}\t'
+                                     f'${round(float(int(postres.get()) * precios_postres[x]),2)}\n')
+        x += 1
+
+    texto_recibo.insert(END, f'-' * 70 + '\n')
+    texto_recibo.insert(END, f'Costo de la comida: \t\t\t{var_costo_comida.get()}\n')
+    texto_recibo.insert(END, f'Costo de las bebidas: \t\t\t{var_costo_bebida.get()}\n')
+    texto_recibo.insert(END, f'Costo de los postres: \t\t\t{var_costo_postre.get()}\n')
+    texto_recibo.insert(END, f'-' * 70 + '\n')
+    texto_recibo.insert(END, f'Subtotal: \t\t\t{var_subtotal.get()}\n')
+    texto_recibo.insert(END, f'Costo impositivo: \t\t\t{var_impuesto.get()}\n')
+    texto_recibo.insert(END, f'Total: \t\t\t{var_total.get()}\n')
+    texto_recibo.insert(END, f'*' * 63 + '\n')
+    texto_recibo.insert(END, f'Lo esperamos pronto!!')
+
+def guardar():
+    info_recibo = texto_recibo.get(1.0, END)
+    archivo = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
+    archivo.write(info_recibo)
+    archivo.close()
+    messagebox.showinfo('Informacion', 'Su recibo ha sido guardado')
+
+
+def resetear():
+    texto_recibo.delete(0.1, END)
+    for texto in texto_comida:
+        texto.set('0')
+    for texto in texto_bebida:
+        texto.set('0')
+    for texto in texto_postres:
+        texto.set('0')
+
+    for cuadro in cuadros_comida:
+        cuadro.config(state=DISABLED)
+    for cuadro in cuadros_bebida:
+        cuadro.config(state=DISABLED)
+    for cuadro in cuadros_postres:
+        cuadro.config(state=DISABLED)
+
+    for variable in variables_comida:
+        variable.set(0)
+    for variable in variables_bebida:
+        variable.set(0)
+    for variable in variables_postres:
+        variable.set(0)
+
+    var_costo_comida.set('')
+    var_costo_bebida.set('')
+    var_costo_postre.set('')
+    var_subtotal.set('')
+    var_impuesto.set('')
+    var_total.set('')
+
+
+# Iniciar tkinter
+aplicacion = Tk()
+
+# Tamaño ventana
+aplicacion.geometry('1200x700+0+0')
+
+# Evitar agrandar pantalla
+aplicacion.resizable()
+
+# Titulo de la ventana
+aplicacion.title("Mi Cantinita - Sistema de Facturación")
+
+# Color fondo
+aplicacion.config(bg='skyblue')
+
+# Panel superior
+panel_superior = Frame(aplicacion, bd=1, relief=FLAT)
+panel_superior.pack(side=TOP)
+
+# Etiqueta titulo
+etiqueta_titulo = Label(panel_superior, text='Sistema de Facturación', fg='azure4',
+                        font=('Dosis', 50), bg='blue', width=27)
+etiqueta_titulo.grid(row=0, column=0)
+
+# panel izquierdo
+panel_izquierdo = Frame(aplicacion, bd=1, relief=FLAT)
+panel_izquierdo.pack(side=LEFT)
+
+# panel costos
+panel_costos = Frame(panel_izquierdo, bd=1, relief=FLAT, bg='azure4', padx=80)
+panel_costos.pack(side=BOTTOM)
+
+
+# panel de comida
+panel_comida = LabelFrame(panel_izquierdo, text='Comida', font=('Dosis', 19, 'bold'),
+                          bd=1, relief=FLAT, fg='azure4')
+panel_comida.pack(side=LEFT)
+
+# panel de bebidas
+panel_bebidas = LabelFrame(panel_izquierdo, text='Bebidas', font=('Dosis', 19, 'bold'),
+                           bd=1, relief=FLAT, fg='azure4')
+panel_bebidas.pack(side=LEFT)
+
+# Panel postres
+panel_postres = LabelFrame(panel_izquierdo, text='Postres', font=('Dosis', 19, 'bold'),
+                           bd=1, relief=FLAT, fg='azure4')
+panel_postres.pack(side=LEFT)
+
+
+# panel derecha
+panel_derecha = Frame(aplicacion, bd=1, relief=FLAT)
+panel_derecha.pack(side=RIGHT)
+
+# panel recibo
+panel_recibo = Frame(panel_derecha, bd=1, relief=FLAT, bg='red')
+panel_recibo.pack()
+
+# panel calculadora
+panel_calculadora = Frame(panel_derecha, bd=1, relief=FLAT, bg='red')
+panel_calculadora.pack()
+
+# panel botones
+panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='red')
+panel_botones.pack()
+
+# lista de productos
+lista_comidas = ['pollo', 'carne', 'ñoquis', 'salmon', 'pizza', 'arroz', 'mariscos', 'sushi']
+lista_bebidas = ['gaseosa', 'jugo', 'soda', 'vino', 'cerveza', 'wisky', 'coñac', 'licuados']
+lista_postres = ['helado', 'fruta', 'flan', 'panqueques', 'mousse', 'pastel', 'brownies', 'donas']
+
+
+"""
+GENERAR ITEMS
+"""
+# Generar items de comida
+variables_comida = []
+cuadros_comida = []
+texto_comida = []
+contador = 0
+for comida in lista_comidas:
+
+    # crear checkbutton
+    variables_comida.append('')
+    variables_comida[contador] = IntVar()
+    comida = Checkbutton(panel_comida, text=comida.title(),
+                         font=('Dosis', 19, 'bold'),
+                         onvalue=1,
+                         offvalue=0,
+                         variable=variables_comida[contador],
+                         command=revisar_check)
+    comida.grid(row=contador,
+                column=0,
+                sticky=W)
+
+    # crear cuadros de entrada
+    cuadros_comida.append('')
+    texto_comida.append('')
+    texto_comida[contador] = StringVar()
+    texto_comida[contador].set('0')
+    cuadros_comida[contador] = Entry(panel_comida,
+                                     font=('Dosis', 18, 'bold'),
+                                     bd=1,
+                                     width=6,
+                                     state=DISABLED,
+                                     textvariable=texto_comida[contador])
+    cuadros_comida[contador].grid(row=contador,
+                                  column=1)
+
+    contador += 1
+
+
+# Generar items de bebida
+variables_bebida = []
+cuadros_bebida = []
+texto_bebida = []
+contador = 0
+for bebida in lista_bebidas:
+    variables_bebida.append('')
+    variables_bebida[contador] = IntVar()
+    bebida = Checkbutton(panel_bebidas,
+                         text=bebida.title(),
+                         font=('Dosis', 19, 'bold'),
+                         onvalue=1,
+                         offvalue=0,
+                         variable=variables_bebida[contador],
+                         command=revisar_check)
+    bebida.grid(row=contador,
+                column=0,
+                sticky=W)
+
+    # crear cuadros de entrada
+    cuadros_bebida.append('')
+    texto_bebida.append('')
+    texto_bebida[contador] = StringVar()
+    texto_bebida[contador].set('0')
+    cuadros_bebida[contador] = Entry(panel_bebidas,
+                                     font=('Dosis', 18, 'bold'),
+                                     bd=1,
+                                     width=6,
+                                     state=DISABLED,
+                                     textvariable=texto_bebida[contador])
+    cuadros_bebida[contador].grid(row=contador,
+                                  column=1)
+    contador += 1
+
+
+# Generar items de postres
+variables_postres = []
+cuadros_postres = []
+texto_postres = []
+contador = 0
+for postres in lista_postres:
+    variables_postres.append('')
+    variables_postres[contador] = IntVar()
+    postres = Checkbutton(panel_postres,
+                          text=postres.title(),
+                          font=('Dosis', 19, 'bold'),
+                          onvalue=1,
+                          offvalue=0,
+                          variable=variables_postres[contador],
+                          command=revisar_check)
+    postres.grid(row=contador,
+                 column=0,
+                 sticky=W)
+
+    # crear cuadros de entrada
+    cuadros_postres.append('')
+    texto_postres.append('')
+    texto_postres[contador] = StringVar()
+    texto_postres[contador].set('0')
+    cuadros_postres[contador] = Entry(panel_postres,
+                                      font=('Dosis', 18, 'bold'),
+                                      bd=1,
+                                      width=6,
+                                      state=DISABLED,
+                                      textvariable=texto_postres[contador])
+    cuadros_postres[contador].grid(row=contador,
+                                   column=1)
+    contador += 1
+
+# Variables
+var_costo_comida = StringVar()
+var_costo_bebida = StringVar()
+var_costo_postre = StringVar()
+var_subtotal = StringVar()
+var_impuesto = StringVar()
+var_total = StringVar()
+
+
+# Etiquetas de costos y campos de entrada comida
+etiqueta_costo_comida = Label(panel_costos,
+                              text='Costo Comida',
+                              font=('Dosis', 12, 'bold'),
+                              bg='azure4',
+                              fg='white')
+etiqueta_costo_comida.grid(row=0, column=0)
+
+texto_costo_comida = Entry(panel_costos,
+                           font=('Dosis', 12, 'bold'),
+                           bd=1,
+                           width=10,
+                           state='readonly',
+                           textvariable=var_costo_comida)
+texto_costo_comida.grid(row=0, column=1, padx=41)
+
+# Etiquetas de costos y campos de entrada bebida
+etiqueta_costo_bebida = Label(panel_costos,
+                              text='Costo Bebida',
+                              font=('Dosis', 12, 'bold'),
+                              bg='azure4',
+                              fg='white')
+etiqueta_costo_bebida.grid(row=1, column=0)
+
+texto_costo_bebida = Entry(panel_costos,
+                           font=('Dosis', 12, 'bold'),
+                           bd=1,
+                           width=10,
+                           state='readonly',
+                           textvariable=var_costo_bebida)
+texto_costo_bebida.grid(row=1, column=1, padx=41)
+
+# Etiquetas de costos y campos de entrada postre
+etiqueta_costo_postre = Label(panel_costos,
+                              text='Costo Postre',
+                              font=('Dosis', 12, 'bold'),
+                              bg='azure4',
+                              fg='white')
+etiqueta_costo_postre.grid(row=2, column=0)
+
+texto_costo_postre = Entry(panel_costos,
+                           font=('Dosis', 12, 'bold'),
+                           bd=1,
+                           width=10,
+                           state='readonly',
+                           textvariable=var_costo_postre)
+texto_costo_postre.grid(row=2, column=1, padx=41)
+
+# Etiquetas de costos y campos de ensubtotal
+etiqueta_subtotal = Label(panel_costos,
+                          text='subtotal',
+                          font=('Dosis', 12, 'bold'),
+                          bg='azure4',
+                          fg='white')
+etiqueta_subtotal.grid(row=0, column=2)
+
+texto_subtotal = Entry(panel_costos,
+                       font=('Dosis', 12, 'bold'),
+                       bd=1,
+                       width=10,
+                       state='readonly',
+                       textvariable=var_subtotal)
+texto_subtotal.grid(row=0, column=3, padx=41)
+
+# Etiquetas de costos y campos de en impuesto
+etiqueta_impuesto = Label(panel_costos,
+                          text='impuestos',
+                          font=('Dosis', 12, 'bold'),
+                          bg='azure4',
+                          fg='white')
+etiqueta_impuesto.grid(row=1, column=2)
+
+texto_impuesto = Entry(panel_costos,
+                       font=('Dosis', 12, 'bold'),
+                       bd=1,
+                       width=10,
+                       state='readonly',
+                       textvariable=var_impuesto)
+texto_impuesto.grid(row=1, column=3, padx=41)
+
+# Etiquetas de costos y campos de en total
+etiqueta_total = Label(panel_costos,
+                       text='total',
+                       font=('Dosis', 12, 'bold'),
+                       bg='azure4',
+                       fg='white')
+etiqueta_total.grid(row=2, column=2)
+
+texto_total = Entry(panel_costos,
+                    font=('Dosis', 12, 'bold'),
+                    bd=1,
+                    width=10,
+                    state='readonly',
+                    textvariable=var_total)
+texto_total.grid(row=2, column=3, padx=41)
+
+# botones
+botones = ['total', 'recibo', 'guardar', 'resetear']
+botones_creados = []
+columnas = 0
+for boton in botones:
+    boton = Button(panel_botones,
+                   text=boton.title(),
+                   font=('Dosis', 14, 'bold'),
+                   fg='white',
+                   bg='azure4',
+                   bd=1,
+                   width=9)
+    botones_creados.append(boton)
+
+    boton.grid(row=0, column=columnas)
+    columnas += 1
+
+botones_creados[0].config(command=total)
+botones_creados[1].config(command=recibo)
+botones_creados[2].config(command=guardar)
+botones_creados[3].config(command=resetear)
+# area recibo
+texto_recibo = Text(panel_recibo,
+                    font=('Dosis', 12, 'bold'),
+                    bd=1,
+                    width=42,
+                    height=20)
+texto_recibo.grid(row=0, column=0)
+
+# calculadora
+visor_calculadora = Entry(panel_calculadora,
+                          font=('Dosis', 12, 'bold'),
+                          bd=1,
+                          width=32)
+
+visor_calculadora.grid(row=0,
+                       column=0,
+                       columnspan=4)
+
+botones_calculadora = ['7', '8', '9', '+',
+                       '4', '5', '6', '-',
+                       '1', '2', '3', 'x',
+                       'R', 'B', '0', '/']
+
+botones_guardados = []
+
+fila = 1
+columna = 0
+for boton in botones_calculadora:
+    boton = Button(panel_calculadora,
+                   text=boton.title(),
+                   font=('Dosis', 16, 'bold'),
+                   fg='white',
+                   bg='azure4',
+                   bd=1,
+                   width=8)
+    botones_guardados.append(boton)
+    boton.grid(row=fila,
+               column=columna)
+    if columna == 3:
+        fila += 1
+
+    columna += 1
+
+    if columna == 4:
+        columna = 0
+
+botones_guardados[0].config(command=lambda : click_boton('7'))
+botones_guardados[1].config(command=lambda : click_boton('8'))
+botones_guardados[2].config(command=lambda : click_boton('9'))
+botones_guardados[3].config(command=lambda : click_boton('+'))
+botones_guardados[4].config(command=lambda : click_boton('4'))
+botones_guardados[5].config(command=lambda : click_boton('5'))
+botones_guardados[6].config(command=lambda : click_boton('6'))
+botones_guardados[7].config(command=lambda : click_boton('-'))
+botones_guardados[8].config(command=lambda : click_boton('1'))
+botones_guardados[9].config(command=lambda : click_boton('2'))
+botones_guardados[10].config(command=lambda : click_boton('3'))
+botones_guardados[11].config(command=lambda : click_boton('*'))
+botones_guardados[12].config(command=obtener_reultado)
+botones_guardados[13].config(command=borrar)
+botones_guardados[14].config(command=lambda : click_boton('0'))
+botones_guardados[15].config(command=lambda : click_boton('/'))
+
+# Evitar que la app se cierre
+aplicacion.mainloop()
 
