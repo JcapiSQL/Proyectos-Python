83ee0798c35efce0f5f40bf2848041734379aedc
CapiDev <juannicolascapitan77@gmail.com>
2024-12-13 18:07:33 -0300

    Create app_logic.py

---
+++ app_logic.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import random
+import datetime
+from tkinter import filedialog, messagebox
+
+def iniciar_aplicacion():
+    aplicacion = Tk()
+    aplicacion.geometry('1200x700+0+0')
+    aplicacion.title("Mi Restó - Sistema de Facturación")
+    aplicacion.config(bg='skyblue')
+    aplicacion.mainloop()
